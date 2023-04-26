b47c8451ba854c29d6aab8948ab452773d9efcc7
DRY-7717 <wicaksanabimaarya01@gmail.com>
2023-04-26 22:38:47 +0700

    redesign submenu admin

--- resources/views/dashboardAdmin/layout.blade.php
+++ resources/views/dashboardAdmin/layout.blade.php
@@ -89,8 +89,8 @@
 
                         <div class="collapse" id="ui-basic">
                             <ul class="nav flex-column sub-menu">
-                                <li class="nav-item ">
-                                    <a class="nav-link" href="{{ route('guru.index') }}">Data Guru
+                                <li class="nav-item">
+                                    <a class="nav-link" href="{{ route('guru.index') }}">Data Bima
                                         <i class="mdi mdi-account-box menu-icon"></i></a>
                                 </li>
                                 <li class="nav-item ">
