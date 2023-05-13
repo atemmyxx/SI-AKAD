93aa2e1c338dff3d2ba63d249f0c86d63475a447
DRY-7717 <wicaksanabimaarya01@gmail.com>
2023-05-14 01:52:09 +0700

    nih udh gw benerin dropdownnya gini aja gabisa...tott tottt...perhatiin lagi versi bootstrapnya

--- resources/views/dashboardAdmin/guru/index.blade.php
+++ resources/views/dashboardAdmin/guru/index.blade.php
@@ -1,40 +1,38 @@
 @extends('dashboardAdmin.layout')
 
 @section('content')
-    <h3 class=" p-2 text-secondary">Data Guru</h3>
-    <div class="table-responsive">
-        <a href="{{ route('guru.create') }}" class="btn btn-primary m-2"><span class="mdi mdi-plus"></span>Tambah Guru</a>
-        <table class="table">
-            <thead>
-                <tr class="table-primary">
-                    <th scope="col">No</th>
-                    <th scope="col">NIP</th>
-                    <th scope="col">Nama</th>
-                    <th scope="col">Jenis Kelamin</th>
-                    <th scope="col">Alamat</th>
-                    <th scope="col">Action</th>
-                </tr>
-            </thead>
-            <tbody>
-                <tr>
-                    <th scope="row">1</th>
-                    <td>123456789</td>
-                    <td>Ahmad Temmy Rietoni S.Kom</td>
-                    <td>Laki-Laki</td>
-                    <td>JL HJ Jaeran, Cinere, Kota Depok</td>
-                    <td>
-                        <div class="dropdown">
-                            <button class="btn btn-secondary dropdown-toggle" type="button" id="dropdownMenuButton"
-                                data-toggle="dropdown" aria-haspopup="true" aria-expanded="false">
-                            </button>
-                            <div class="dropdown-menu" aria-labelledby="dropdownMenuButton">
-                                <a class="dropdown-item" href="#">Action</a>
-                                <a class="dropdown-item" href="#">Another action</a>
-                                <a class="dropdown-item" href="#">Something else here</a>
-                            </div>
-                        </div>
-                    </td>
-                </tr>
-        </table>
-    </div>
+<h3 class=" p-2 text-secondary">Data Guru</h3>
+<div class="table-responsive">
+    <a href="{{ route('guru.create') }}" class="btn btn-primary m-2"><span class="mdi mdi-plus"></span>Tambah Guru</a>
+    <table class="table">
+        <thead>
+            <tr class="table-primary">
+                <th scope="col">No</th>
+                <th scope="col">NIP</th>
+                <th scope="col">Nama</th>
+                <th scope="col">Jenis Kelamin</th>
+                <th scope="col">Alamat</th>
+                <th scope="col">Action</th>
+            </tr>
+        </thead>
+        <tbody>
+            <tr>
+                <th scope="row">1</th>
+                <td>123456789</td>
+                <td>Ahmad Temmy Rietoni S.Kom</td>
+                <td>Laki-Laki</td>
+                <td>JL HJ Jaeran, Cinere, Kota Depok</td>
+                <td>
+                    <button class="btn btn-secondary dropdown-toggle" type="button" id="dropdownMenuButton1"
+                        data-bs-toggle="dropdown" aria-expanded="false">
+                        Aksi
+                    </button>
+                    <ul class="dropdown-menu" aria-labelledby="dropdownMenuButton1">
+                        <li><a class="dropdown-item" href="#">Action</a></li>
+                    </ul>
+
+                </td>
+            </tr>
+    </table>
+</div>
 @endsection
